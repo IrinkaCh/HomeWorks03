23e3e686fb98787f575a276a242ca4471fa84c0c
IrinkaCh <123irina00@mail.ru>
2022-08-13 23:34:40 +0500

    Task 03 Find Cube Number

---
+++ Task03FindCubeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindCubeNumber(N));
+
+string FindCubeNumber(int N)
+{
+string result = "";
+for (int i = 1; i <= N; i++)
+{
+    result += $"{Math.Pow(i,3)} ";
+}
+  return result;
+}
